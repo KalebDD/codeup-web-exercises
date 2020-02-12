9ae131e199515859ff90a7c43e5ef789cdf3936d
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-02-11 18:16:37 -0600

    completed second for loop exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -10,5 +10,18 @@ showMultiplicationTable(7);
 
 //Q.3
 function tenRandomNumbers() {
+    let randomNumber = Math.floor(Math.random() * 180) + 20;
 
+    for (i = 1; i <= 10; i++) {
+        if (randomNumber % 2 === 0) {
+            console.log(randomNumber + " is even.");
+            randomNumber = Math.floor(Math.random() * 180) + 20
+        } else {
+            console.log(randomNumber + " is odd.");
+            randomNumber = Math.floor(Math.random() * 180) + 20
+        }
+    }
 }
+tenRandomNumbers();
+
+//Q.4
