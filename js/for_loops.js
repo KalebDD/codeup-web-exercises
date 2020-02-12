ee8c366586a6099c0ff58c0f6ffae4a1e1991866
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-02-11 18:06:06 -0600

    completed first for loop exercise

---
+++ js/for_loops.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//Q.2
+function showMultiplicationTable(num) {
+    for (i = 1; i <= 10; i++) {
+        console.log(num + " x " + i + " = " + num*i);
+    }
+}
+showMultiplicationTable(7);
+
+//Q.3
+function tenRandomNumbers() {
+
+}
