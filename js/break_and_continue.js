0202e82ef8502ac3ad6198ffff2cd8cbb5f7688a
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-02-12 09:22:31 -0600

    finished all exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,3 +1,20 @@
 "use strict";
 
 var userNumber = prompt("Please enter an odd number between 1 and 50.");
+
+while (isNaN(parseFloat(userNumber)) || userNumber % 2 === 0) {
+    userNumber = prompt("Please enter an odd number between 1 and 50.");
+    if (!isNaN(parseFloat(userNumber)) && userNumber % 2 === 1) {
+        break;
+    }
+}
+
+console.log("Number to skip is: " + userNumber);
+
+for (i = 1; i < 50; i += 2) {
+    if (parseFloat(userNumber) === i) {
+        console.log("Yikes! Skipping number " + i);
+        continue;
+    }
+    console.log("Here is an odd number: " + i);
+}
