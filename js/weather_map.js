774cb719274f7b2d1005ed2eeeb2dc4a993ee44c
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-03-10 22:20:49 -0500

    refactored 2 expected weather functions into 1

--- js/weather_map.js
+++ js/weather_map.js
@@ -34,8 +34,14 @@ $(document).ready(function () {
 
     //Toggle # of days forecast
     var numberOfDays = 3;
-    $('.threeDays').click(function () {numberOfDays = 3; renderEverything()});
-    $('.sevenDays').click(function () {numberOfDays = 7; renderEverything()});
+    $('.threeDays').click(function () {
+        numberOfDays = 3;
+        renderEverything()
+    });
+    $('.sevenDays').click(function () {
+        numberOfDays = 7;
+        renderEverything()
+    });
 
     //Main function
     function renderEverything() {
@@ -43,7 +49,10 @@ $(document).ready(function () {
         $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/" + marker.getLngLat().lat + "," + marker.getLngLat().lng).done(function (data) {
             // console.log(new Date(data.currently.time * 1000));
             console.log(data);
-            reverseGeocode({lat: marker.getLngLat().lat, lng: marker.getLngLat().lng}, mapboxToken).then(function(location) {
+            reverseGeocode({
+                lat: marker.getLngLat().lat,
+                lng: marker.getLngLat().lng
+            }, mapboxToken).then(function (location) {
                 $('.current-weather').html(currentWeather(data, location));
                 $('.expected-weather').html(expectedWeather(data));
             })
@@ -51,8 +60,8 @@ $(document).ready(function () {
     }
 
     //Current weather render
-    function currentWeather(input, location) {
-        var weatherText = input.currently;
+    function currentWeather(weatherData, location) {
+        var weatherText = weatherData.currently;
 
         var html = '<div class="card text-center"> <div class="card-body">';
         html += '<h3 class=card-title ">' + location + '</h3>';
@@ -67,22 +76,45 @@ $(document).ready(function () {
     }
 
     //Expected weather render
-    function expectedWeather(input) {
+//     function expectedWeather(input) {
+//         var html = '';
+//         for (var i = 0; i < numberOfDays; i++) {
+//             html += weatherCards(input.daily.data[i]);
+//         }
+//         return html;
+//     }
+//     function weatherCards(input) {
+//         var html = '<div class="card"> <div class=card-body>';
+//         html += '<h4 class="card-title d-flex justify-content-between"><span>' + input.temperatureLow + '&#730</span><span>' + input.temperatureHigh + '&#730</span></h4>';
+//         html += '<h3 class="card-text">' + input.summary + '</h3>';
+//         html += '<h3><b>Humidity:</b> ' + input.humidity + '</h3>';
+//         html += '<h3 class="card-text"><b>Wind:</b> ' + input.windGust + '</h3>';
+//         html += '<h3 class="card-text"><b>Pressure:</b> ' + input.pressure + '</h3>';
+//         html += '</div> </div>';
+//         return html;
+//     }
+//     renderEverything();
+// });
+
+
+    function expectedWeather(weatherData) {
         var html = '';
+        var weather = weatherData.daily.data;
+
         for (var i = 0; i < numberOfDays; i++) {
-            html += weatherCards(input.daily.data[i]);
+            console.log(weather);
+            html += '<div class="card"> <div class=card-body>';
+            html += '<h4 class="card-title d-flex justify-content-between"><span>' + weather[i].temperatureLow + '&#730</span><span>' + weather[i].temperatureHigh + '&#730</span></h4>';
+            html += '<h3 class="card-text">' + weather[i].summary + '</h3>';
+            html += '<h3><b>Humidity:</b> ' + weather[i].humidity + '</h3>';
+            html += '<h3 class="card-text"><b>Wind:</b> ' + weather[i].windGust + '</h3>';
+            html += '<h3 class="card-text"><b>Pressure:</b> ' + weather[i].pressure + '</h3>';
+            html += '</div> </div>';
         }
         return html;
     }
-    function weatherCards(input) {
-        var html = '<div class="card"> <div class=card-body>';
-        html += '<h4 class="card-title d-flex justify-content-between"><span>' + input.temperatureLow + '&#730</span><span>' + input.temperatureHigh + '&#730</span></h4>';
-        html += '<h3 class="card-text">' + input.summary + '</h3>';
-        html += '<h3><b>Humidity:</b> ' + input.humidity + '</h3>';
-        html += '<h3 class="card-text"><b>Wind:</b> ' + input.windGust + '</h3>';
-        html += '<h3 class="card-text"><b>Pressure:</b> ' + input.pressure + '</h3>';
-        html += '</div> </div>';
-        return html;
-    }
+
+
+
     renderEverything();
 });
